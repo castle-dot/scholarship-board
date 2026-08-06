84b2cc0018d986867dfc7fb404db3f4c01601834
castle-dot <caslte2wonderland@gmail.com>
2026-08-06 12:00:40 +0300

    add STATIC_ROOT

--- scholarship_web/settings.py
+++ scholarship_web/settings.py
@@ -21,6 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
+# SECURITY WARNING: keep the secret key used in production secret!
+
+
 
 SECRET_KEY = os.environ.get("SECRET_KEY", "dev-only-insecure-key-change-me")
 DEBUG = os.environ.get("DEBUG", "True") == "True"
@@ -115,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"  
 LOGIN_REDIRECT_URL = "scholarship-list"
 LOGOUT_REDIRECT_URL = "scholarship-list"
